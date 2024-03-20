30123e54ee0c0188b1a3d92c31e418da3fd07e26
MaxRU <m_rubcov@bk.ru>
2024-03-21 00:09:46 +0300

    adding branches

--- src/main/java/ru/netology/javaqadiplom/Account.java
+++ src/main/java/ru/netology/javaqadiplom/Account.java
@@ -5,26 +5,32 @@ public class Account {
     protected int rate;
 
     public boolean pay(int amount) {
+
         return false;
     }
 
     public boolean add(int amount) {
+
         return false;
     }
 
     public int yearChange() {
+
         return 0;
     }
 
     public int getBalance() {
+
         return balance;
     }
 
     public int getRate() {
+
         return rate;
     }
 
     public void setRate(int rate) {
+
         this.rate = rate;
     }
 }
