4294cb711b129f44cb044e9def4df9c1e5677507
MaxRU <m_rubcov@bk.ru>
2024-03-22 00:45:19 +0300

    adding Tests Saving

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -14,8 +14,92 @@ public class SavingAccountTest {
                 5
         );
 
-        account.add(3_000);
+        account.add(8_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        Assertions.assertEquals(2_000 + 8_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldNotAddNegativeNumber() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(-100);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldNotAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(10_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayMoreThanMinBalance() {
+        SavingAccount saving = new SavingAccount(
+                1200,
+                1_000,
+                10_000,
+                5
+        );
+
+        boolean expected = false;
+        boolean actual = saving.pay(500);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPayLessThanMinBalance() {
+        SavingAccount saving = new SavingAccount(
+                1600,
+                1_000,
+                10_000,
+                5
+        );
+
+        boolean expected = true;
+        boolean actual = saving.pay(500);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPayEqualsThanMinBalance() {
+        SavingAccount saving = new SavingAccount(
+                1500,
+                1_000,
+                10_000,
+                5
+        );
+
+        boolean expected = false;
+        boolean actual = saving.pay(500);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldChangeBalance() {
+        SavingAccount saving = new SavingAccount(
+                1500,
+                1_000,
+                10_000,
+                5
+        );
+
+        int expected = 75;
+        int actual = saving.yearChange();
+        Assertions.assertEquals(expected, actual);
     }
 }
